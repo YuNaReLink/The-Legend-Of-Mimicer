dbbd3682a1939165cd8175571b799ecf65e65b7a
MOTOHISA <link080514@gmail.com>
2024-08-11 17:11:49 +0900

    アイテム作成

--- Assets/Script/GameObject/Item/Urns/BleakUrns.cs
+++ Assets/Script/GameObject/Item/Urns/BleakUrns.cs
@@ -7,10 +7,40 @@ public class BleakUrns : MonoBehaviour
 
     [SerializeField]
     private ObjectList list = null;
+    public enum ItemNumber
+    {
+        Null = -1,
+        Heart,
+        DataEnd
+    }
+
+    public enum RandomRatio
+    {
+        Null = -1,
+        Low,
+        Middle,
+        High,
+        DataEnd
+    }
+
+    [SerializeField]
+    private int[] ratios = new int[]
+    {
+        100,
+        500,
+        1000,
+    };
+
+    [SerializeField]
+    private RandomRatio ratioNum = RandomRatio.Low;
 
     private void AppearItem()
     {
-
+        int ratio = Random.Range(0,ratios[(int)ratioNum]);
+        if(ratio < ratios[(int)ratioNum] / 2)
+        {
+            Instantiate(list.Objects[(int)ItemNumber.Heart],transform.position,Quaternion.identity);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
